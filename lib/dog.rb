6f5fdc0fbe2fd7cea39010e91e82012f8dc7f1bf
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-03-03 14:09:16 -0500

    commit tree 100b819c1f3ffa7007810ad00cdd46ecd248fd0b

---
+++ lib/dog.rb
@@ -0,0 +1,48 @@
+class Dog
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+
+
+
+end
+
+
+fido = Dog.new
+fido.bark
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+#     def bark
+#         puts "Woof!"
+    
+#         def sit 
+#             puts "The Dog is sitting"
+
+#         end
+#     end
+# end
+
+# fido = Dog.new
+# fido.bark
+# fido.sit
+
+# snoopy = Dog.new
+# snoopy.bark
